976c1ae29e250158d04f56ab04a5c1fa013e71fe
Sebastien Ahkrin <sebastien48criquet@gmail.com>
2021-03-05 14:51:14 +0100

    refactor: remove unused comments
    Fixes #6

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
 export * from './WellPicker';
 export * from './WellPlate';
 export { PositionFormat } from 'well-plates';
-
-// displayAsGrid
-// add row and col on wellStyle
-// wellplate.getPosition() => row and col
